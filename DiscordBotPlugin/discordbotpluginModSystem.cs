d6a1b76df1fa7c0819d04954582e54d53cc41af8
noshpineapple28 <manoucherin@gmail.com>
2026-01-30 11:52:00 -0500

    initial commit

---
+++ DiscordBotPlugin/discordbotpluginModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace DiscordBotPlugin
+{
+    public class DiscordBotPluginModSystem : ModSystem
+    {
+        // Called on server and client
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + Lang.Get("discordbotplugin:hello"));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side");
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side");
+        }
+    }
+}
